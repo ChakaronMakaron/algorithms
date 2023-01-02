0d53fdc70e30e321d31b0f9032d19b473fe3cd97
le makhno <voroshilov-klim@ukr.net>
2023-01-03 00:03:43 +0200

    quick commit

--- src/algorithms/dynamic/programming/DynamicProgramming_LongestIncreasingSubs.java
+++ src/algorithms/dynamic/programming/DynamicProgramming_LongestIncreasingSubs.java
@@ -1,14 +1,15 @@
 package algorithms.dynamic.programming;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.max;
+
 import java.util.Arrays;
-import java.util.Collections;
 
 public class DynamicProgramming_LongestIncreasingSubs {
     
     public static int longestIncSubsRecursive(Integer[] sequence, int n) {
-        System.out.println(Arrays.toString(sequence));
         if (n == sequence.length - 1) {
-            if (sequence[n] >= sequence[n - 1]) {
+            if (sequence[n] > sequence[n - 1]) {
                 return 2;
             }
             return 1;
@@ -16,25 +17,19 @@ public class DynamicProgramming_LongestIncreasingSubs {
 
         Integer[] res = new Integer[sequence.length];
         Arrays.fill(res, 0);
-
-        System.out.println("i: " + sequence[n]);
         
         for (int i = n; i < res.length; i++) {
-            int increment = 0;
-            if (sequence[n - 1] <= sequence[n]) {
-                increment++;
-                System.out.println("i-1: " + sequence[n - 1]);
-                
-            }
+            int increment = sequence[n - 1] < sequence[n] ? 1 : 0;
             res[n - 1] = longestIncSubsRecursive(sequence, n + 1) + increment;
         }
             
-        return Collections.max(Arrays.asList(res));
+        return max(asList(res));
     }
 
     public static void main(String[] args) {
         Integer[] seq = {2, 4, 3, 5, 1, 7, 6, 9, 8};
-        //Integer[] seq = {2, 3, 5, 6, 7, 8};
+        //Integer[] seq2 = {1, 0};
+        //Integer[] seq1 = {2, 3, 5, 6, 7, 8};
 
         System.out.println(longestIncSubsRecursive(seq, 1));
     }
