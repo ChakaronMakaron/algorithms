2b395cc8623ada37ee3971a1c706ead0d17b261f
le makhno <voroshilov-klim@ukr.net>
2023-01-02 04:23:45 +0200

    quick commit

--- src/algorithms/dynamic/programming/DynamicProgramming_StringCompare.java
+++ src/algorithms/dynamic/programming/DynamicProgramming_StringCompare.java
@@ -2,15 +2,16 @@ package algorithms.dynamic.programming;
 
 import static java.util.Collections.min;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class DynamicProgramming_StringCompare {
  
-    static int editDist(String str1, String str2, int m, int n) {
+    public static int editDist(String str1, String str2, int m, int n) {
         if (m == 0) return n;
         if (n == 0) return m;
 
-        if (str1.charAt(m) == str2.charAt(n))
+        if (str1.charAt(m - 1) == str2.charAt(n - 1))
             return editDist(str1, str2, m - 1, n - 1);
         
         return 1 + min(List.of(
@@ -20,12 +21,44 @@ public class DynamicProgramming_StringCompare {
         ));
     }
 
-    public static void main(String args[])
-    {
-        String str1 = "sunday";
-        String str2 = "saturday";
- 
-        System.out.println(editDist(
-            str1, str2, str1.length() - 1, str2.length() - 1));
+    public static void main(String args[]) {
+        String str1 = "you should not";
+        String str2 = "thou shalt not";
+
+        System.out.println(editDist(str1, str2, str1.length(), str2.length()));
+    }
+
+    public static int editDistDP(String str1, String str2) {
+        str1 = " " + str1;
+        str2 = " " + str2;
+
+        // Memo init
+        int[][] memo = new int[str1.length()][str2.length()];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i][0] = i;
+        }
+        for (int j = 0; j < memo[0].length; j++) {
+            memo[0][j] = j;
+        }
+
+        for (int i = 1; i < str1.length(); i++) {
+            for (int j = 1; j < str2.length(); j++) {
+                memo[i][j] = min(List.of(
+                    memo[i - 1][j - 1] + (str1.charAt(i) == str2.charAt(j) ? 0 : 1),
+                    memo[i - 1][j] + 1,
+                    memo[i][j - 1] + 1
+                ));
+                printArr(memo);
+                System.out.println("----------------------");
+            }
+        }
+
+        return memo[memo.length - 1][memo[0].length - 1];
+    }
+
+    private static void printArr(int[][] arr) {
+        for (int[] is : arr) {
+            System.out.println(Arrays.toString(is));
+        }
     }
 }
