9c1060fc63dd5d92cb057ab3c44ae4f7ece5c3d7
le makhno <voroshilov-klim@ukr.net>
2023-01-05 02:22:08 +0200

    quick commit

--- src/algorithms/dynamic/programming/DynamicProgramming_LongestIncreasingSubs.java
+++ src/algorithms/dynamic/programming/DynamicProgramming_LongestIncreasingSubs.java
@@ -4,33 +4,34 @@ import static java.util.Arrays.asList;
 import static java.util.Collections.max;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class DynamicProgramming_LongestIncreasingSubs {
     
     public static int longestIncSubsRecursive(Integer[] sequence, int n) {
-        if (n == sequence.length - 1) {
-            if (sequence[n] > sequence[n - 1]) {
-                return 2;
+        if (n == 0) {return 1;}
+
+        Integer[] table = new Integer[sequence.length];
+        Arrays.fill(table, 0);
+
+        for (int i = n - 1; i > 0; i--) {
+            table[i] = longestIncSubsRecursive(sequence, i);
+            if (sequence[n] > sequence[i]) {
+                table[i] = table[i] + 1;
             }
-            return 1;
         }
 
-        Integer[] res = new Integer[sequence.length];
-        Arrays.fill(res, 0);
-        
-        for (int i = n; i < res.length; i++) {
-            int increment = sequence[n - 1] < sequence[n] ? 1 : 0;
-            res[n - 1] = longestIncSubsRecursive(sequence, n + 1) + increment;
-        }
-            
-        return max(asList(res));
+        return max(asList(table));
     }
 
     public static void main(String[] args) {
-        Integer[] seq = {2, 4, 3, 5, 1, 7, 6, 9, 8};
+        //Integer[] seq = {2, 4, 3, 5, 1, 7, 6, 9, 8};
         //Integer[] seq2 = {1, 0};
-        //Integer[] seq1 = {2, 3, 5, 6, 7, 8};
+        //Integer[] seq1 = {2, 3, 5, 6, 7, 8, 9};
+        Integer[] seq3 = {5, 4, 1, 9, 8, 7};
 
-        System.out.println(longestIncSubsRecursive(seq, 1));
+        Integer[] seq44 = {4,10,4,3,8,9};
+
+        System.out.println(longestIncSubsRecursive(seq44, seq44.length - 1));
     }
 }
